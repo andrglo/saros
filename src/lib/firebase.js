086b7449f1334f3b0cba1e0ec8d8fdc845200ebd
Andre Gloria <andredagloria@gmail.com>
2019-07-23 12:47:37 -0300

    Use env file in root dir

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,7 +4,7 @@ import 'firebase/database'
 import 'firebase/auth'
 import 'firebase/storage'
 
-import env from '../.saros.env'
+import env from '../../.saros.env'
 
 firebase.initializeApp(env.firebaseConfig)
 
