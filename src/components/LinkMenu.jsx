df0aba5b97a4104c3f8827c8e18cf3d021038296
Andre Gloria <andredagloria@gmail.com>
2019-07-30 14:57:59 -0300

    Support keyboard navigation in menu

--- src/components/LinkMenu.jsx
+++ src/components/LinkMenu.jsx
@@ -1,10 +1,12 @@
-import React, {useRef, useEffect} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 import Link from './Link'
 
+const getItemId = i => `link_${i}`
+
 const Menu = props => {
-  const {className, options, onClose} = props
+  const {className, options, onClose, focus} = props
   const overlayRef = useRef(null)
   useEffect(() => {
     const type = 'click'
@@ -20,17 +22,38 @@ const Menu = props => {
       document.onkeydown = onKeyDown
     }
   }, [])
+  const [focused, setFocus] = useState(focus ? 0 : -1)
+  useEffect(() => {
+    const item = document.getElementById(getItemId(focused))
+    if (item) {
+      item.focus()
+    }
+  }, [focused])
+  let i = -1
   return (
     <React.Fragment>
       <div
         className="fixed h-screen w-screen inset-0"
         ref={overlayRef}
       />
-      <nav
+      <div
         className={cn(
           'bg-menu text-menu rounded shadow-md z-30',
           className
         )}
+        role="menu"
+        onClick={onClose}
+        onKeyDown={event => {
+          if (event.key === 'ArrowDown') {
+            if (focused < i) {
+              setFocus(focused + 1)
+            }
+          } else if (event.key === 'ArrowUp') {
+            if (focused > 0) {
+              setFocus(focused - 1)
+            }
+          }
+        }}
       >
         {options.map(option => {
           const {label, link, icon, divider} = option
@@ -38,7 +61,13 @@ const Menu = props => {
             divider || (
               <Link
                 key={label}
+                id={getItemId(++i)}
                 to={link}
+                onFocus={() => {
+                  if (focused < 0) {
+                    setFocus(0)
+                  }
+                }}
                 className="bg-menu text-menu hover:bg-menuHover p-2 w-full text-left block hover:no-underline flex justify-start items-center mb-1 rounded-sm"
               >
                 {icon}
@@ -47,7 +76,7 @@ const Menu = props => {
             )
           )
         })}
-      </nav>
+      </div>
     </React.Fragment>
   )
 }
@@ -55,6 +84,7 @@ const Menu = props => {
 Menu.propTypes = {
   className: PropTypes.string,
   onClose: PropTypes.func.isRequired,
+  focus: PropTypes.bool,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       label: PropTypes.string,
