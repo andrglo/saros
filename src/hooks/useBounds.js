02449d672903f0baa78cfa8173999456fbe6b6b5
Andre Gloria <andredagloria@gmail.com>
2019-08-21 13:24:21 -0300

    Fix bounds update

--- src/hooks/useBounds.js
+++ src/hooks/useBounds.js
@@ -5,12 +5,17 @@ import getScrollParent from '../lib/getScrollParent'
 const useBounds = ref => {
   const [bounds, setBounds] = useState()
   const node = ref && 'current' in ref ? ref.current : ref
+  const {left, right, top, bottom, width, height} =
+    (node && node.getBoundingClientRect()) || {}
+  useEffect(() => {
+    setBounds({left, right, top, bottom, width, height})
+  }, [bottom, height, left, node, right, top, width])
+
   const updateBounds = () => {
     if (node) {
       setBounds(node.getBoundingClientRect())
     }
   }
-  useEffect(updateBounds, [node])
   useEventListener('resize', updateBounds)
   useEventListener('scroll', updateBounds, getScrollParent(node))
   return bounds
