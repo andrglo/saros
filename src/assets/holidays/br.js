ad27ec316ccb4d22476568d916b6a00ec17cbd57
Andre Gloria <andredagloria@gmail.com>
2019-08-22 16:27:35 -0300

    Loader for countries

---
+++ src/assets/holidays/br.js
@@ -0,0 +1,51 @@
+module.exports = {
+  national: {
+    '01-01': {
+      name: 'Confraternização Universal'
+    },
+    '04-21': {
+      name: 'Tiradentes'
+    },
+    '09-07': {
+      name: 'Independência'
+    },
+    '10-12': {
+      name: 'Nossa Senhora Aparecida'
+    },
+    '11-02': {
+      name: 'Finados'
+    },
+    '11-15': {
+      name: 'Proclamação da república'
+    },
+    '12-25': {
+      name: 'Natal'
+    },
+    'easter-48': {
+      name: 'Carnaval' // segunda
+    },
+    'easter-47': {
+      name: 'Carnaval' // terça
+    },
+    'easter-2': {
+      name: 'Sexta-feira Santa'
+    },
+    'easter+60': {
+      name: 'Corpus Christi' // quinta
+    }
+  },
+  states: {
+    MG: {
+      regions: {
+        'Belo Horizonte': {
+          '08-15': {
+            name: 'Assunção de Nossa Senhora'
+          },
+          '12-08': {
+            name: 'Imaculada Conceição'
+          }
+        }
+      }
+    }
+  }
+}
--- src/selectors/atlas.js
+++ src/selectors/atlas.js
@@ -1,19 +1,18 @@
 import {defaultMemoize as memoize} from 'reselect'
-import Holidays from '../lib/date-holidays'
+// import {LocalDate} from 'js-joda'
+// import calc from 'date-easter'
 
-// date-holidays is a build from https://github.com/commenthol/date-holidays with fewer countries
-
-const hd = new Holidays()
+import atlas from '../loaders/atlas!'
 
 let countries
 
 export const getCountries = () => {
   if (!countries) {
     countries = []
-    const data = hd.getCountries()
+    const data = atlas.countries
     for (const key of Object.keys(data)) {
       countries.push({
-        label: data[key],
+        label: data[key].nativeName,
         value: key
       })
     }
@@ -23,30 +22,30 @@ export const getCountries = () => {
 
 export const getStates = memoize(country => {
   const states = []
-  const data = hd.getStates(country)
-  if (!data) {
-    return states
-  }
-  for (const key of Object.keys(data)) {
-    states.push({
-      label: data[key],
-      value: key
-    })
-  }
+  // const data = hd.getStates(country)
+  // if (!data) {
+  //   return states
+  // }
+  // for (const key of Object.keys(data)) {
+  //   states.push({
+  //     label: data[key],
+  //     value: key
+  //   })
+  // }
   return states
 })
 
 export const getCities = memoize((country, state) => {
   const cities = []
-  const data = hd.getRegions(country, state)
-  if (!data) {
-    return cities
-  }
-  for (const key of Object.keys(data)) {
-    cities.push({
-      label: data[key],
-      value: key
-    })
-  }
+  // const data = hd.getRegions(country, state)
+  // if (!data) {
+  //   return cities
+  // }
+  // for (const key of Object.keys(data)) {
+  //   cities.push({
+  //     label: data[key],
+  //     value: key
+  //   })
+  // }
   return cities
 })
