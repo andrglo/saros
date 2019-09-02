e08ae9c8e29d11b0242e272b50c16dd44d31c9fc
Andre Gloria <andredagloria@gmail.com>
2019-09-02 16:48:50 -0300

    btn as btn-default

--- plugins/tailwind-buttons-plugin.js
+++ plugins/tailwind-buttons-plugin.js
@@ -1,7 +1,24 @@
 module.exports = utils => {
   const {theme, addComponents} = utils
-  const baseColors = ['default', 'primary', 'secondary', 'tertiary']
+  const baseColors = ['primary', 'secondary', 'tertiary']
   const {colors = []} = theme('buttons', {})
+  const insertColor = color => ({
+    backgroundColor: `var(--color-bg-${color})`,
+    color: `var(--color-text-${color})`,
+    '&:hover': {
+      backgroundColor: `var(--color-bg-${color}-hover)`,
+      color: `var(--color-text-${color}-hover)`
+    },
+    '&:disabled': {
+      backgroundColor: `var(--color-highlight)`,
+      filter: 'invert(50%)',
+      cursor: 'not-allowed'
+    },
+    '&:active': {
+      backgroundColor: `var(--color-bg-${color}-active)`,
+      color: `var(--color-text-${color}-active)`
+    }
+  })
   const buttons = {
     '.btn': {
       padding: '.25rem .75rem',
@@ -10,31 +27,17 @@ module.exports = utils => {
       borderWidth: 1,
       fontWeight: '500',
       textTransform: 'uppercase',
-      boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      boxShadow:
+        '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       '&:focus': {
         outline: 'none',
         boxShadow: '0 0 0 3px rgba(66, 153, 225, 0.5)'
-      }
+      },
+      ...insertColor('default')
     }
   }
   for (const color of [...baseColors, ...colors]) {
-    buttons[`.btn-${color}`] = {
-      backgroundColor: `var(--color-bg-${color})`,
-      color: `var(--color-text-${color})`,
-      '&:hover': {
-        backgroundColor: `var(--color-bg-${color}-hover)`,
-        color: `var(--color-text-${color}-hover)`,
-      },
-      '&:disabled': {
-        backgroundColor: `var(--color-highlight)`,
-        filter: 'invert(50%)',
-        cursor: 'default'
-      },
-      '&:active': {
-        backgroundColor: `var(--color-bg-${color}-active)`,
-        color: `var(--color-text-${color}-active)`,
-      }
-    }
+    buttons[`.btn-${color}`] = insertColor(color)
   }
   addComponents(buttons)
 }
