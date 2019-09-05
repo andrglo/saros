d78f2d73cc3eb8a9b71dcfcadfb9a430d8c87fad
Andre Gloria <andredagloria@gmail.com>
2019-09-05 16:28:39 -0300

    Date lib to wrap js-joda

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,5 +1,9 @@
 import {LocalDate, YearMonth, TemporalAdjusters} from '@js-joda/core'
 
+export const isYearMonth = date => date && date.length === 7
+export const extractYearMonth = date => date && date.substring(0, 7)
+export const today = () => LocalDate.now().toString()
+
 export const toYearMonth = date => {
   let result
   try {
@@ -8,11 +12,30 @@ export const toYearMonth = date => {
           date.length === 7 ? date : date.substring(0, 7)
         )
       : YearMonth.now()
+    result = result.toString()
   } catch (err) {
     console.error('toYearMonth', date, err)
-    result = YearMonth.now()
+    result = YearMonth.now().toString()
   }
   return result
+}
+
+export const addMonths = (date, months) => {
+  try {
+    date = date || today()
+    if (isYearMonth(date)) {
+      date = YearMonth.parse(date)
+        .plusMonths(months)
+        .toString()
+    } else {
+      date = LocalDate.parse(date)
+        .plusMonths(months)
+        .toString()
+    }
+  } catch (err) {
+    console.error('addMonths', date, err)
+  }
+  return date
 }
 
 export const startOfMonth = date => {
@@ -25,4 +48,12 @@ export const startOfMonth = date => {
   return date
 }
 
-export const today = () => LocalDate.now().toString()
+export const endOfMonth = date => {
+  try {
+    date = date ? LocalDate.parse(date) : LocalDate.now()
+    date = date.with(TemporalAdjusters.lastDayOfMonth()).toString()
+  } catch (err) {
+    console.error('endOfMonth', date, err)
+  }
+  return date
+}
