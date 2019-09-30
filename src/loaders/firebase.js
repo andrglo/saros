80fae1bb445b678d8c721d4a455d0df77d14d761
Andre Gloria <andredagloria@gmail.com>
2019-09-30 15:01:05 -0300

    Avoid firebase deprecated cli command

--- src/loaders/firebase.js
+++ src/loaders/firebase.js
@@ -4,7 +4,9 @@ let firebaseConfig
 
 if (process.env.NODE_ENV !== 'test') {
   const {execSync} = require('child_process')
-  const stdout = execSync('firebase setup:web')
+  const stdout = execSync(
+    `firebase apps:sdkconfig web ${process.env.FIREBASE_APP_ID}`
+  )
   const match = stdout
     .toString()
     .match(/firebase\.initializeApp\(([^)]+)\)/m)
