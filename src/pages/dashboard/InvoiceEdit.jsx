131efb4be363a1889a87ef1f99f151f7cffb5e72
Andre Gloria <andredagloria@gmail.com>
2019-10-17 16:14:14 -0300

    Add form to invoice edit

--- src/pages/dashboard/InvoiceEdit.jsx
+++ src/pages/dashboard/InvoiceEdit.jsx
@@ -1,37 +1,67 @@
 import React from 'react'
+import {hot} from 'react-hot-loader/root'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 import {connect} from 'react-redux'
 import debug from 'debug'
 
-import {getDb} from '../../selectors/app'
+import Form from '../../components/Form'
+
+import t from '../../lib/translate'
+import {makeFormName} from '../../selectors/forms'
+import {
+  getCollectionFullName,
+  getInvoices
+} from '../../selectors/docs'
+import SmartInput from '../../components/SmartInput'
+
+const form = 'invoice:edit'
 
 // eslint-disable-next-line no-unused-vars
-const log = debug('invoice:edit')
+const log = debug(form)
 
 const InvoiceEdit = props => {
-  const {className, query, db} = props
+  const {className, query, collection} = props
   log('render', props)
-  const {id} = query
-  // const formName = `invoiceEdit@${db}#${id}`
+  const {id, isNew} = query
+  const formName = makeFormName(form, collection, id)
+  const title = t`Transaction`
+  // toBeContinued...
   return (
-    <div className={cn('text-center text-xl italic pt-2', className)}>
-      <p>InvoiceEdit (todo)</p>
-    </div>
+    <React.Fragment>
+      <p className="title">{title}</p>
+      <Form
+        className={cn(
+          'mx-auto max-w-xl grid grid-gap-1 pt-4 p-1 grid-columns-1 sm:grid-columns-3 lg:grid-columns-4',
+          className
+        )}
+        formName={formName}
+        title={title}
+        collection={collection}
+        selector={getInvoices}
+        id={id}
+        isNew={isNew === 'true'}
+      >
+        <SmartInput id="notes" />
+      </Form>
+    </React.Fragment>
   )
 }
 
 InvoiceEdit.propTypes = {
   className: PropTypes.string,
-  db: PropTypes.string.isRequired,
+  collection: PropTypes.string.isRequired,
   query: PropTypes.shape({
-    id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired,
+    isNew: PropTypes.string
   }).isRequired
 }
 
-export default connect(state => {
-  const db = getDb(state)
-  return {
-    db
-  }
-})(InvoiceEdit)
+export default hot(
+  connect(state => {
+    const collection = getCollectionFullName(state, 'invoices')
+    return {
+      collection
+    }
+  })(InvoiceEdit)
+)
