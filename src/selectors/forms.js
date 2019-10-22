f0b440907007f1cabaddc93fad07285c605b8ecd
Andre Gloria <andredagloria@gmail.com>
2019-10-22 14:22:11 -0300

    Check if form exists

--- src/selectors/forms.js
+++ src/selectors/forms.js
@@ -2,12 +2,12 @@ import {createSelector} from '../lib/reselect'
 
 export const getForms = state => state.forms
 export const getForm = (state, {formName}) => state.forms[formName]
-export const getFormValues = form => form.values
-export const getFormInitialValues = form => form.initialValues
-export const getFieldErrors = form => form.fieldErrors
-export const getFormIsDirty = form => form.dirty
-export const getFormUndo = form => form.undo
-export const getFormLock = form => form.lock
+export const getFormValues = form => form && form.values
+export const getFormInitialValues = form => form && form.initialValues
+export const getFieldErrors = form => form && form.fieldErrors
+export const getFormIsDirty = form => form && form.dirty
+export const getFormUndo = form => form && form.undo
+export const getFormLock = form => form && form.lock
 
 export const getDirtyForms = createSelector(
   getForms,
